e946fabd608737f2953ffe7f19f35f52edd1a431
Jaykumar Halpati <rameshhalpati007@gmail.com>
2020-12-05 18:55:01 +0000

    Added Axios

--- src/search.js
+++ src/search.js
@@ -8,51 +8,26 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Paper from './paper';
 import './App.css';
 import Box from '@material-ui/core/Box';
+import axios from 'axios';
 
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
 
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
 
-function sleep(delay = 0) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  });
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }}))
-
-export default function Asynchronous() {
-  const [open, setOpen] = React.useState(false);
-  const [options, setOptions] = React.useState([]);
-  const loading = open && options.length === 0;
-  const classes = useStyles();
-
-
-
-
-  React.useEffect(() => {
-    let active = true;
-
-  
-
-    (async () => {
-      const response = await fetch('https://local-authority-eng.register.gov.uk/records.json?page-size=5000');
-      await sleep(1e3); // For demo purposes.
-      const counties = await response.json();
-      console.log(counties);
-
-    })();
-  
-  }, [loading]);
-
-
-
-  return(
-    <div><p>Ello</p></div>
-
-  );
-
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
 }
