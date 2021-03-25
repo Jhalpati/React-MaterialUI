c4a2b47c3e09217cf43e492bd8421d71abc38004
Jaykumar Halpati <rameshhalpati007@gmail.com>
2021-03-25 19:56:04 +0000

    Added React hooks & search functionality

--- src/history.js
+++ src/history.js
@@ -1,10 +1,8 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import Button from '@material-ui/core/Button';
+
 import SearchBar from "material-ui-search-bar";
+import { ContactSupportOutlined } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -16,62 +14,91 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-export default class FetchRandomUser extends React.Component {
+function History()
+ {
 
   
-  state = {
-    eventos: [],
+  const [data, setItems] = useState([]);
+  // search state
+  const [search, setSearch] = useState();
+  // Default query state
+  const [query, setQuery] = useState('12/12');
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  // useEffect() function
+  useEffect(() => {
+    // fetchItems() makes an asyncronous call
+    const fetchItems = async () => {
+      setLoading(true);
+      setError(false);
+
+      try {
+        const response = await fetch(
+          // Proxy was used to make this API call
+          //`https://cors-anywhere.herokuapp.com/https://api.flightstats.com/flex/delayindex/rest/v1/json/region/Asia?appId=${APP_ID}&appKey=${APP_KEY}&classification=5&score=3`
+          `https://byabbe.se/on-this-day/${query}/events.json`
+        );
+
+        // Response received in JSON format and  stored as const data
+        const data = await response.json();
+        console.log(data);
+        // Sets response as data.airports
+        setItems(data);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
+  // Function to
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    // Resets the input
+    setSearch('');
   };
-
-componentDidMount() {
-
-  // GET request using fetch with error handling
-  fetch('https://byabbe.se/on-this-day/12/14/events.json')
-      .then(async response => {
-          const data = await response.json();
-          console.log(data.events);
-
-          // check for error response
-          if (!response.ok) {
-              // get error message from body or default to response statusText
-              const error = (data && data.message) || response.statusText;
-              return Promise.reject(error);
-          }
-
-          this.setState({ totalReactPackages: data.total })
-      })
-      .catch(error => {
-          this.setState({ errorMessage: error.toString() });
-          console.error('There was an error!', error);
-      });
-}
+  
+const updateSearch = (e) => {
+  setSearch(e.target.value);
+};
 
 
 
 
 
-  render(){
+
+
     return(
       
       <div>
 
-<SearchBar
-      onChange={() => console.log('onChange')}
-      onRequestSearch={() => console.log('onRequestSearch')}
+    <SearchBar
+          onSubmit={getSearch}
+          value={search}
+      // onChange={() => console.log('onRequestSearch1')}
+      
+      onRequestSearch={updateSearch}
       style={{
         margin: '20px auto',
         maxWidth: 800
       }}
     />
 
+    <h1>Hello</h1>
+
       </div>
 
 
 
 
-    )}
+    )
 }
+
+export default History;
 
 {/* <SearchBar
 onChange={() => console.log('onChange')}
